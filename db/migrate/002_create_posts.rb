20b2c58033096d0ce934951e3e7baa558258e35c
JMoloughney <github email address>
2018-06-06 15:40:27 -0400

    completed basic backend setup w/ auth - tested

--- db/migrate/002_create_posts.rb
+++ db/migrate/002_create_posts.rb
@@ -3,8 +3,6 @@ class CreatePosts< ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.string :title
       t.string :body
-      t.string :img_url
-      t.string :article_url
       t.integer :user_id
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1
-user2
-user3
-user4
-user5
-user6
-user7
+user1 = User.create(username: 'Test', password:'Test')
+
+post1 = Post.create(title: 'test', body: 'testing posts', user_id: 1)
