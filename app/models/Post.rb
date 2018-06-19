fbff236882aa441a71c8e6ce4706041dcfae4c73
JMoloughney <github email address>
2018-06-18 22:29:22 -0400

    added a filter so noone can post foul words-well atleast it covers them

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
+  require 'obscenity/active_model'
+  
   belongs_to :user
+
+  validates :title, presence: true
+  # validates :url, url: { allow_blank: true }
+  validates :title,  obscenity: { sanitize: true, replacement: "@!#%"}
+  validates :body,  obscenity: { sanitize: true, replacement: "@!#%"}
+
+
 end
